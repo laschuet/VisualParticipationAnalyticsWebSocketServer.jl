16931feddd4db31acaf2036bd511c94ff2630eb7
laschuet <laschuet@users.noreply.github.com>
2021-03-12 15:07:33 +0100

    Asynchronously run submitted jobs

--- src/websocket.jl
+++ src/websocket.jl
@@ -1,17 +1,43 @@
+const JOBS = Channel{String}(32)
+const OUT = Channel{String}(32)
+
+"""
+    runjobs()
+"""
+function runjobs()
+    while true
+        job = take!(JOBS)
+        if job == "job_rand_sleep"
+            put!(OUT, """{ "payload": "Job started. This may take some time..." }""")
+            result = 3 * rand()
+            sleep(result)
+            put!(OUT, """{ "payload": $result }""")
+        end
+    end
+end
+
 """
     start(port::Integer)
 """
-function start(port::Integer=3020)
+function start(port::Integer=3020, nthreads=Threads.nthreads())
     @async HTTP.listen(HTTP.Sockets.localhost, port) do http
         if HTTP.WebSockets.is_upgrade(http.message)
             HTTP.WebSockets.upgrade(http) do ws
-                while !eof(ws);
-                    msg = String(readavailable(ws))
-                    println(msg)
-                    write(ws, msg)
+                @sync begin
+                    @async while !eof(ws)
+                        msg = String(readavailable(ws))
+                        put!(JOBS, msg)
+                    end
+                    @async while isopen(ws)
+                        write(ws, take!(OUT))
+                    end
                 end
             end
         end
     end
-    println("WebSocket server is running at http://localhost:$port")
+    println("WebSocket server is running at http://$(HTTP.Sockets.localhost):$port")
+
+    for _ = 1:nthreads
+        @async runjobs()
+    end
 end
